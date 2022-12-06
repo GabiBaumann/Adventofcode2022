3288b64f844f4a3d56d7aa20d4744b6b47630647
Andre Müller <andre.mueller@gesis.org>
2022-12-06 22:12:40 +0100

    Day 6 doesn't need fiddling with strings
    Come to think of it...
    Nice :)
    
    Changes to be committed:
    	new file:   Day06.1.2-Tuning_Trouble-No_String.py

---
+++ Day06.1.2-Tuning_Trouble-No_String.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# SPDX-License-Identifier: GPL-3.0-only
+
+## Length of helper string with all unique characters required.
+## That's signal length minus one, as one more is always worked on.
+## So, use strlength = 3 for 6.1, strlength = 13 for 6.2
+#strlength = 3
+strlength = 13
+
+clear_at = 0
+pos = 0
+
+with open('Day06-Input', 'r') as file:
+    stream = file.read()
+
+for next_one in stream:
+    pos += 1
+    for i in range(strlength):
+        if pos < i:
+            break
+        elif next_one == stream[pos - i - 2]:
+            clear_at = max(clear_at, pos - i - 1)
+            break
+    if clear_at < pos - strlength: # this is it
+        print(pos, stream[pos-strlength-1:pos])
+        break
