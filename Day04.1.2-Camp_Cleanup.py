110552a232363f106aa5560fbb81e6522ad41fba
Andre Müller <andre.mueller@gesis.org>
2022-12-12 16:43:56 +0100

    Add back documentation bits for Part 2
    I accidentally removed it along with the file.
    
    Changes to be committed:
    	modified:   Day04.1.2-Camp_Cleanup.py

--- Day04.1.2-Camp_Cleanup.py
+++ Day04.1.2-Camp_Cleanup.py
@@ -49,7 +49,25 @@ In how many assignment pairs does one range fully contain the other?
 """
 
 """
-For each line's pair of ranges, find the max of start numbers and the min of end numbers. If both are in the same tuple, increase the counter.
+It seems like there is still quite a bit of duplicate work planned. Instead, the Elves would like to know the number of pairs that overlap at all.
+
+In the above example, the first two pairs (2-4,6-8 and 2-3,4-5) don't overlap, while the remaining four pairs (5-7,7-9, 2-8,3-7, 6-6,4-6, and 2-6,4-8) do overlap:
+
+    5-7,7-9 overlaps in a single section, 7.
+    2-8,3-7 overlaps all of the sections 3 through 7.
+    6-6,4-6 overlaps in a single section, 6.
+    2-6,4-8 overlaps in sections 4, 5, and 6.
+
+So, in this example, the number of overlapping assignment pairs is 4.
+"""
+
+
+
+"""
+Part 1: For each line's pair of ranges, find the max of start numbers and the min of end numbers. If both are in the same tuple, increase the counter.
+
+
+Part 2: Get the max startpoint, look if it's less than or equal to min endpoint.
 """
 
 count1 = 0
