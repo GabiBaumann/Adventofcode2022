62d4797ab4e9333131fe570c89fd0c253e38c007
Andre Müller <andre.mueller@gesis.org>
2022-12-12 16:55:55 +0100

    Forgot the Doc for Part 2.
    Added.
    
    Changes to be committed:
    	modified:   Day12.2-Hill_Climbing-Scenic.py

--- Day12.2-Hill_Climbing-Scenic.py
+++ Day12.2-Hill_Climbing-Scenic.py
@@ -62,13 +62,41 @@ What is the fewest steps required to move starting from any square with elevatio
 """
 
 """
+--- Part Two ---
+
+As you walk up the hill, you suspect that the Elves will want to turn this into a hiking trail. The beginning isn't very scenic, though; perhaps you can find a better starting point.
+
+To maximize exercise while hiking, the trail should start as low as possible: elevation a. The goal is still the square marked E. However, the trail should still be direct, taking the fewest steps to reach its goal. So, you'll need to find the shortest path from any square at elevation a to the square marked E.
+
+Again consider the example from above:
+
+Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi
+
+Now, there are six choices for starting position (five marked a, plus the square marked S that counts as being at elevation a). If you start at the bottom-left square, you can reach the goal most quickly:
+
+...v<<<<
+...vv<<^
+...v>E^^
+.>v>>>^^
+>^>>>>>^
+
+This path reaches the goal in only 29 steps, the fewest possible.
+
+What is the fewest steps required to move starting from any square with elevation a to the location that should get the best signal?
+"""
+
+"""
 This is about path optimisation.
 Do the next step recursively, trying each direction at any step.
 Output minimum steps taken.
 
-Record the ordinal values (shifted - 96) in the first place. Start with '`' (96) as character before 'a', end with '{' (123) as character after 'z'. At height 27: we've found one way to go.
+Record the ordinal values (shifted - 96) in the first place. End with '{' (123) as character after 'z'. At height 27: we've found one way to go.
 
-This solution needs a list of coordinates visited already to not run in loops. This needs to be local to the recursion branch.
+This solution needs an array to keep track of how many steps it took to get to this position. If it gets there faster, set value to current number of steps.
 """
 
 from sys import setrecursionlimit
