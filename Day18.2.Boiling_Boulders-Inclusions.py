564ae73447c0aa6e72712857dca2efc992155817
Andre Müller <andre.mueller@gesis.org>
2022-12-19 21:31:22 +0100

    Day 18.2: Works fine now.
    Caviry removal did the trick.
    Recursion wasn't actually needed, tho.
    
    Changes to be committed:
    	modified:   Day18.2.Boiling_Boulders-Inclusions.py

--- Day18.2.Boiling_Boulders-Inclusions.py
+++ Day18.2.Boiling_Boulders-Inclusions.py
@@ -17,7 +17,31 @@ for each point in each range, look if a bigger one exists and a smaller one exis
 Also, do the 17.1 dance.
 For all found, do the 17.1 dance again on that list.
 Subtract the latter from the former.
+
+Well... there are cavities.
+If anyone in inclusion has any surface not connecting to more inclusion
+or cube, remove that, and any inclusion that connects to it. recurse.
 """
+
+def remove_cavity(cube):
+    "Remove this item and all neighbors in cavity recursively."
+    x, y, z = cube[0], cube[1], cube[2]
+    print("Remove from cavity:", cube)
+    print(inclusions)
+    inclusions.remove(cube)
+    if (x-1, y, z) in inclusions:
+        remove_cavity((x-1, y, z))
+    if (x+1, y, z) in inclusions:
+        remove_cavity((x+1, y, z))
+    if (x, y-1, z) in inclusions:
+        remove_cavity((x, y-1, z))
+    if (x, y+1, z) in inclusions:
+        remove_cavity((x, y+1, z))
+    if (x, y, z-1) in inclusions:
+        remove_cavity((x, y, z-1))
+    if (x, y, z+1) in inclusions:
+        remove_cavity((x, y, z+1))
+
 
 surface = 0
 cubes = []
@@ -26,8 +50,8 @@ inclusions = []
 minx = miny = minz = 100
 maxx = maxy = maxz = 0
 
-#with open('Day18-Input', 'r') as file:
-with open('Day18-Input--Debug', 'r') as file:
+with open('Day18-Input', 'r') as file:
+#with open('Day18-Input--Debug', 'r') as file:
     for line in file:
         x, y, z = line.strip().split(',')
         x = int(x)
@@ -35,7 +59,7 @@ with open('Day18-Input--Debug', 'r') as file:
         z = int(z)
         minx = min(minx, x)
         miny = min(miny, y)
-        minz = min(minz, y)
+        minz = min(minz, z)
         maxx = max(maxx, x)
         maxy = max(maxy, y)
         maxz = max(maxz, z)
@@ -62,9 +86,9 @@ for cube in cubes:
 for cx in range(minx, maxx+1):
     for cy in range(miny, maxy+1):
         for cz in range(minz, maxz+1):
-            print("Testing", cx, cy, cz)
+            #print("Testing", cx, cy, cz)
             if (cx, cy, cz) in cubes:
-                print("Is in cubes")
+                #print("Is in cubes")
                 continue
             lower_x = False
             lower_y = False
@@ -91,26 +115,69 @@ for cx in range(minx, maxx+1):
                 if (cx, cy, i) in cubes:
                     higher_z = True
             if lower_x == higher_x == lower_y == higher_y == lower_z == higher_z == True:
-                print("We've got an inclusion")
+                #print("We've got an inclusion")
                 inclusions.append( (cx,cy,cz) )
 
-print(inclusions)
+# remove any inclusion that has a direct neighbour that's not in inclusion
+# or cube.
+# recurse for all it's neighbours in inclusion.
+cavities = []
+for cube in inclusions:
+    x, y, z = cube[0], cube[1], cube[2]
+    cavity = False
+    if (x-1, y, z) not in inclusions and (x-1, y, z) not in cubes:
+        print("Wolf, wolf, x-1")
+        cavity = True
+    if (x+1, y, z) not in inclusions and (x+1, y, z) not in cubes:
+        print("Wolf, wolf, x+1")
+        cavity = True
+    if (x, y-1, z) not in inclusions and (x, y-1, z) not in cubes:
+        print("Wolf, wolf, y-1")
+        cavity = True
+    if (x, y+1, z) not in inclusions and (x, y+1, z) not in cubes:
+        print("Wolf, wolf, y+1")
+        cavity = True
+    if (x, y, z-1) not in inclusions and (x, y, z-1) not in cubes:
+        print("Wolf, wolf, z-1")
+        cavity = True
+    if (x, y, z+1) not in inclusions and (x, y, z+1) not in cubes:
+        print("Wolf, wolf, z+1")
+        cavity = True
+    if cavity:
+        cavities.append( (x, y, z) )
+
+for i in cavities:
+    remove_cavity(i)
+    
+
 inner_surface = 0
 for cube in inclusions:
     s = 6
     x, y, z = cube[0], cube[1], cube[2]
     if (x-1, y, z) in inclusions:
         s -= 1
+    elif (x-1, y, z) not in cubes:
+        print("Wolf!!!", x, y, z, "x-1 not in cubes")
     if (x+1, y, z) in inclusions:
         s -= 1
+    elif (x+1, y, z) not in cubes:
+        print("Wolf!!!", x, y, z, "x+1 not in cubes")
     if (x, y-1, z) in inclusions:
         s -= 1
+    elif (x, y-1, z) not in cubes:
+        print("Wolf!!!", x, y, z, "y-1 not in cubes")
     if (x, y+1, z) in inclusions:
         s -= 1
+    elif (x, y+1, z) not in cubes:
+        print("Wolf!!!", x, y, z, "y+1 not in cubes")
     if (x, y, z-1) in inclusions:
         s -= 1
+    elif (x, y, z-1) not in cubes:
+        print("Wolf!!!", x, y, z, "z-1 not in cubes")
     if (x, y, z+1) in inclusions:
         s -= 1
+    elif (x, y, z+1) not in cubes:
+        print("Wolf!!!", x, y, z, "z+1 not in cubes")
     inner_surface += s
 
 print(surface)
@@ -120,6 +187,7 @@ surface -= inner_surface
 
 print(surface)
 
+# 2588 it is.
 # 2576 is too low
 # 58 is the example result.
 
