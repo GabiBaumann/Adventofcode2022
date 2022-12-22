f4573a5debec029595a1e7a408d90b545b0045ff
Andre Müller <andre.mueller@gesis.org>
2022-12-22 18:15:56 +0100

    Day 22.2: Urx.
    Detailing what wraps to where in what direction in the description.
    No clue how this shall be coded.
    
    Perhaps stop at wrap, change direction, get new coordinates,
    do rest of steps as a new entry? May even append to list at beginning.
    ...and instead of counting, pop(0) after finishing a move.
    
    Changes to be committed:
    	modified:   Day22.2-Monkey_Map-Cube.py

--- Day22.2-Monkey_Map-Cube.py
+++ Day22.2-Monkey_Map-Cube.py
@@ -79,6 +79,136 @@ In the above example, the final row is 6, the final column is 8, and the final f
 Follow the path given in the monkeys' notes. What is the final password?
 """
 
+"""
+As you reach the force field, you think you hear some Elves in the distance. Perhaps they've already arrived?
+
+You approach the strange input device, but it isn't quite what the monkeys drew in their notes. Instead, you are met with a large cube; each of its six faces is a square of 50x50 tiles.
+
+To be fair, the monkeys' map does have six 50x50 regions on it. If you were to carefully fold the map, you should be able to shape it into a cube!
+
+In the example above, the six (smaller, 4x4) faces of the cube are:
+
+        1111
+        1111
+        1111
+        1111
+222233334444
+222233334444
+222233334444
+222233334444
+        55556666
+        55556666
+        55556666
+        55556666
+
+You still start in the same position and with the same facing as before, but the wrapping rules are different. Now, if you would walk off the board, you instead proceed around the cube. From the perspective of the map, this can look a little strange. In the above example, if you are at A and move to the right, you would arrive at B facing down; if you are at C and move down, you would arrive at D facing up:
+
+        ...#
+        .#..
+        #...
+        ....
+...#.......#
+........#..A
+..#....#....
+.D........#.
+        ...#..B.
+        .....#..
+        .#......
+        ..C...#.
+
+Walls still block your path, even if they are on a different face of the cube. If you are at E facing up, your movement is blocked by the wall marked by the arrow:
+
+        ...#
+        .#..
+     -->#...
+        ....
+...#..E....#
+........#...
+..#....#....
+..........#.
+        ...#....
+        .....#..
+        .#......
+        ......#.
+
+Using the same method of drawing the last facing you had with an arrow on each tile you visit, the full path taken by the above example now looks like this:
+
+        >>v#
+        .#v.
+        #.v.
+        ..v.
+...#..^...v#
+.>>>>>^.#.>>
+.^#....#....
+.^........#.
+        ...#..v.
+        .....#v.
+        .#v<<<<.
+        ..v...#.
+
+The final password is still calculated from your final position and facing from the perspective of the map. In this example, the final row is 5, the final column is 7, and the final facing is 3, so the final password is 1000 * 5 + 4 * 7 + 3 = 5031.
+
+Fold the map into a cube, then follow the path given in the monkeys' notes. What is the final password?
+"""
+
+"""
+Wrap <-
+y= 0 - 49, x = 50 and (moving left)
+to y= 149 - 100, x=0  (moving right)
+
+Wrap ->
+y= 0-49, x = 149 and (moving right)
+to y = 149 - 100, x=99 (moving left)
+
+Wrap <-
+y= 50-99, x=50 (moving left)
+to x= 0-49, y= 100 (moving down)
+
+Wrap ->
+y= 50-99, x=99 (moving right)
+to x= 100-149, y=49 (moving up)
+
+Wrap <-
+y= 100-149, x=0 (moving left)
+to y= 49-0, x=50 (moving right)
+
+Wrap ->
+y= 100-149 x=99 (moving right)
+to y= 49-0, x= 149 (moving left)
+
+Wrap <-
+y= 150-199, x=0 (moving left)
+to x= 50-99, y=0 (moving down)
+
+Wrap ->
+y= 150-199, x=49 (moving right)
+to x= 50-99, y=149 (moving up)
+
+Wrap ^
+x= 50-99, y=0 (moving up)
+to y = 150-199, x=0 (moving right)
+
+Wrap ^
+x= 100-149, y=0 (moving up)
+to x= 0-50, y = 199 (moving up)
+
+Wrap v
+x= 100-149, y=49 (moving down)
+to y= 50-99, x=99 (moving left)
+
+Wrap ^
+x= 0-49, y=100 (moving up)
+to y=50-99, x=50 (moving right)
+
+Wrap v
+x=50-99, y-149 (moving down)
+to y= 150-199, x=49 (moving left)
+
+Wrap v
+x=0-49, y= 199 (moving down)
+to x=100-149, y=0 (moving down)
+"""
+
 initmap = []
 move = []
 direction = []
